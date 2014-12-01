4450ec4342c58fcb0062298172a6dfd1c1ecbbbb
Dave Lunny <dlunny@featurecreeptechnology.com>
2014-11-30 22:53:02 -0500

    major restructuring
    Just need the links to work.

--- app/js/app.js
+++ app/js/app.js
@@ -29,7 +29,6 @@ app.config(function($stateProvider, $urlRouterProvider) {
         templateUrl: "partials/desktop.html",
         controller: 'DesktopCtrl'
       })
-
 });
 
 //  Not much to see here, other than the sweet redirect to desktop if needed
@@ -82,31 +81,58 @@ app.controller('Controller', function ($scope, $state, $log) {
 //  this where the goods at
 app.controller('MobileCtrl', function ($scope, $state, $log, $http){
 
-    //  Prepping
+    //  These are the two data objects
     $scope.regions = [];
     $scope.routes = {};
+
+
     $scope.routesList = [];
-    $scope.currentRoutes = [];
+    $scope.activeRoutes = [];
     $scope.selected = {};
 
-    //  Lets get route data plz
-    $http.get('js/routes.json')
-      .success(function(data, status){
-          $scope.regions = data.regions;
-          $scope.routes = data.routes;
-      })
-      .error(function(data, status){
-          throw "Error: Could not retrieve route data from routes.json";
-      });
+    //  check if we already grabbed that route data
+   /* if(localStorage.routesData){
 
-    //  This is what populates the activeRoutes
-    $scope.$watch( 'selected.region', function(){
-        if( $scope.selected.region != undefined ){
+      var data = JSON.parse(localStorage.routesData);
+      var now = new Date,
+          now = Date.parse()
+      //  if the data hasn't been reset in awhile, go ahead and re-grab it
+      if( daysSince(data.date, now) > 30 ){
+        $log.log('Resetting le data, its been > 30 days!')
+        getRouteData($http, $scope);
+      }else{
+        //  Regions is an array of region names, capitalized
+        $scope.regions = data.regions;
+        //  routes is an object which contains each of the region's associated routes
+        $scope.routes = data.routes;
+      }
 
+    }else{*/
+      getRouteData($http, $scope);
+    /*
+    };//end getting/setting route data*/
+
+    ////////////////////////////////////////////
+    //                STEP ONE                //
+    //            Select a region             //
+    ////////////////////////////////////////////
+
+    $scope.pickedNewRegion = function(){
+
+      //  reset 'child' selections to undefinded
+      $scope.selected.route = undefined;
+      $scope.selected.directional = undefined;
+      $scope.selected.routePickedAlready = undefined;
+      $scope.selected.showBtns = undefined;
+
+      if( $scope.selected.region != undefined ){
+          //  our selected region needs to be lowercased cause its stored capitalized
           var selReg = $scope.selected.region.toLowerCase();
-          $scope.currentRoutes = $scope.routes[selReg];
+          //  set the active routes object
+          $scope.activeRoutes = $scope.routes[selReg];
 
           //  to reset the routes list
+          //  routesList is a human-readable list of
           $scope.routesList = [];
           //  loop through each route that matches that region
           $.each( $scope.routes[selReg], function(i, val){
@@ -114,19 +140,83 @@ app.controller('MobileCtrl', function ($scope, $state, $log, $http){
           });
 
         }//end if there is a selected region
-    });
+    };
 
-    $scope.$watch( 'selected.route', function(){
-        if( $scope.selected.route != undefined ){
+    ////////////////////////////////////////////
+    //                STEP TWO                //
+    //            Select a route              //
+    ////////////////////////////////////////////
 
-          //  at this point we're determining the date
-            //  TODO move this up, as it will be done only once when the app loads
-            //  perhaps not because we need to know if the current day is even possible
-          $scope.daysList = setDatesList( $scope.currentRoutes, $scope.selected.route );
+    $scope.pickedNewRoute = function(){
+      //  reset 'child' selections to undefinded
+      $scope.selected.directional = undefined;
+      $scope.selected.routePickedAlready = undefined;
+      $scope.selected.showBtns = undefined;
+
+      //  reset daysList, which becomes the list of avail days
+      //  TODO: if there is only one available, then just show the btns
+      $scope.daysList = [];
+
+      //  If they actually picked something
+      if( $scope.selected.route != undefined ){
+
+          //  get out days list, as well as set the...
+          //  THIS SHOULD RETURN THE OBJECT THAT IS THE SELECTED ROUTE, AND
+          $scope.routeData = setProperRoute( $scope.selected.route, $scope.activeRoutes );
+
+          //  if we need to deal with direction first
+          if($scope.routeData.direction){
+            $scope.selected.directional = true;
+            $scope.dirList = $scope.routeData.directions;
+            // $scope.selected.routePickedAlready = undefined; // undeed cause done above
+          }else{
+            $scope.selected.routePickedAlready = true;
+            //  Set the dayslist to that one
+            $scope.daysList = $scope.routeData.sched.days_list;
+          }
+
+      }
+    };
+
+    ////////////////////////////////////////////
+    //               STEP TWO (B)             //
+    //            Select a direction          //
+    ////////////////////////////////////////////
+
+    $scope.pickedNewDirection = function(){
+      $scope.selected.showBtns = undefined;
+      if( $scope.selected.direction != undefined ){
+
+        //  this is the direction, lowercased for object notation.
+        var d = $scope.selected.direction.toLowerCase();
+        //  this sets the list to that specific direction, as done above
+        $scope.daysList = $scope.routeData[d].days_list;
+
+        $scope.selected.routePickedAlready = true;
+
+      }
+    };
 
 
-        }
-    });
+    ////////////////////////////////////////////
+    //               STEP THREE               //
+    //              Select a day              //
+    ////////////////////////////////////////////
+
+    $scope.pickedNewDay = function(){
+      if( $scope.selected.day != undefined ){
+
+        $scope.activeSched = configSchedUrl( $scope.routeData, $scope.selected.region  );
+
+        //  Show butns now that the url is configed.
+        $scope.selected.showBtns = true;
+      }
+    };
+
+
+
+
+
 
 });
 
@@ -141,23 +231,65 @@ app.controller('DesktopCtrl', function ($scope, $state, $log){
 });
 
 
-var setDatesList = function( routes, selected ) {
-  var days = [];
+//  takes the selected "### - Route Name" style name and spits out the 
+var setProperRoute = function( dirty, allRoutes ){
+  //  just the route number
+  //  TODO: when there is no route number...
+  //  TODO: when there is something like "255A" or "123B"
+  var clean = parseInt(dirty), obj;
 
-  $.each( routes, function(i, val){
-    //  TODO: make route numbers intergers so that you dont have to parse int
-    if( parseInt(val.num) === parseInt(selected)  ){
-
-          // check what days it has available
-
-          //  also a good time to check if we need to throw the directional flag to grab that input
-
+  //  Loop through the routes for that region, and pick out the one with the matching route number
+  $.each( allRoutes, function(i, val){
+    //  TODO: make the route numbers intergers cause for no need to parseInt them
+    //      BUT WAIT: if they are ints, you gotta account for "255A", like above...
+    if( parseInt(val.num) === clean ){
+      obj = val;
     }
   });
 
-  return days;
+  return obj;
 };
 
+
+//  Gets the route data and stores it in localstorage, as well as a current date stamp
+var getRouteData = function($http, $scope){
+
+  //  Lets get the route data, and y'all know we gon save it in localstorage to save time later
+  $http.get('js/routes.json')
+    .success(function(data, status){
+        //  Regions is an array of region names, capitalized
+        $scope.regions = data.regions;
+        //  routes is an object which contains each of the region's associated routes
+        $scope.routes = data.routes;
+        var d = new Date();
+        //  add a date stamp to this bitttch
+        data.date = Date.parse(d);
+
+        //  store data in localStorage
+        localStorage.routesData = JSON.stringify(data);
+    })
+    .error(function(data, status){
+        throw "Error: Could not retrieve route data from routes.json";
+    });
+};
+
+//  returns the number of days since then, from now
+var daysSince = function(then, now){
+  return Math.floor(( now - then ) / 86400000);
+};
+
+//  Configures the url to the pdf where our sched is located at
+var configSchedUrl = function( routeData, region  ){
+
+  var ifDir = routeData.direction ? $scope.selected.direction.toLowerCase()+'/' : '';
+
+
+  var url = '../public/routes/' + region.toLowerCase() + '/' + routeData.num + '/' ;
+
+  console.log( url );
+
+  return url;
+};
 
 
 
